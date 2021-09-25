3c3c1a0c818c4eb8ea18fa831474693b04108bc9
tamon-persol <60681425+tamon-persol@users.noreply.github.com>
2021-09-25 15:26:37 +0900

    feat add map component

--- components/MapComponent/MapComponent.tsx
+++ components/MapComponent/MapComponent.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactMapGl from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const mapBoxToken =
-  'pk.eyJ1IjoidGFtb25hYSIsImEiOiJja3R6N201enUzYmZqMzBtcDRzc200ZjZpIn0.dOSiDM21rDLUqvU_kCyrdg';
+  'API Tokken';
 
 export type ViewPortType = {
   width: string;
@@ -13,14 +14,22 @@ export type ViewPortType = {
   zoom: number;
 };
 
-export const MapComponent = () => {
-  const [state, setState] = useState({
+export type State = {
+  viewport: ViewPortType;
+};
+
+export type MapComponentProps = {
+  zoom?: number;
+};
+
+export const MapComponent = (props: MapComponentProps) => {
+  const [state, setState] = useState<State>({
     viewport: {
       width: '100vw',
       height: '100vh',
-      latitude: 40.78343,
-      longitude: -73.96625,
-      zoom: 11
+      latitude: 35.75127833344205,
+      longitude: 139.77243871559978,
+      zoom: props.zoom || 10
     }
   });
 
